db8e243665660ee472b565660aaaf3f3f3887451
polinavenberg <venberg1976@gmail.com>
2021-04-04 21:49:13 +0300

    Added main programm function

---
+++ Encrypte.py
@@ -0,0 +1,15 @@
+import collections
+from copy import deepcopy
+
+from functions import caesar_analysys
+from functions import caesar_cipher
+from functions import de_caesar_cipher
+from functions import de_morse_code
+from functions import de_vegenere_cipher
+from functions import de_vernam_cipher
+from functions import morse_code
+from functions import vegenere_cipher
+from functions import vernam_cipher
+from start import start
+
+start()
