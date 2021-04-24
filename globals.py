f399eeffbe68387d31007b6bcc503ddc420b0275
polinavenberg <venberg1976@gmail.com>
2021-04-24 17:36:31 +0300

    Added new globals

--- globals.py
+++ globals.py
@@ -12,5 +12,21 @@ class Globals:
              'z': '——••'}
     degree = 2
     most_common_letter = 'e'
+    forward_step = 1
+    back_step = -1
     random_letter = 'a'
     BMP_HEADER_SIZE = 54
+
+    divide_by_two = 2
+    to_window_center_width = 340
+    to_window_center_high = 260
+
+    bits = 8
+    last_byte = 0b00000000
+    text_mask = 0b11111111
+    img_mask = 0b11111111
+    step = 1
+    first_bit = 0
+    last_bit = 8
+    empty_value = 0
+    number_of_bits = 256
